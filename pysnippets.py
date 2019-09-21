1e8c9739300c9970164a5697c8575619635e3ef0
manukris <manukrishnatesro@gmail.com>
2019-09-21 09:40:04 +0530

    secound commit

--- pysnippets.py
+++ pysnippets.py
@@ -1,1 +1,37 @@
 #python useful snippets
+
+########################################################################################################################
+# program to find anagram
+from collections import Counter
+
+def anagram(first, second):
+    return Counter(first) == Counter(second)
+
+
+print(anagram("abcd3", "3acdb")) # True
+
+##########################################################################################################################
+# normal program for anagrams My Logic
+
+word1 = input("Enter a word1")
+word2 = input("Enter a word2")
+n1    = len(word1)
+count = 0
+for char in word1:
+    if char in word2:
+        count += 1
+else:
+    if count == n1:
+        print("anagram")
+    else:
+        print("not a anagram")
+
+
+
+
+
+
+
+
+
+
